9bda32ba5f7de8f406a099061e73b07793105eee
sanghyeonchoi <76832243+sanghyeonchoi@users.noreply.github.com>
2021-04-21 14:34:26 +0900

    Create baekjoon5622.py

---
+++ baekjoon5622.py
@@ -0,0 +1,9 @@
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a = input()
+time = 0
+for i in range(len(a)):
+    for j in dial:
+        if a[i] in j:
+            time += dial.index(j)+3
+
+print(time)
