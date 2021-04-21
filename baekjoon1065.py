04ec674fbe24a5cf7874e60757200e0d39fd9578
sanghyeonchoi <76832243+sanghyeonchoi@users.noreply.github.com>
2021-04-21 12:21:05 +0900

    Create baekjoon1065.py
    한수가 뭔지 문제부터 어려움

---
+++ baekjoon1065.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+num = int(input())
+count = 0
+
+for n in range(1, num+1):
+    if n <= 99:
+        count += 1
+    else:
+        nums = list(map(int, str(n)))
+        if nums[0] - nums[1] == nums[1] - nums[2]:
+            count += 1
+
+print(count)
