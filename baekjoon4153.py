255713c3be9ad616ccc90db1d3bf4c1f90edaf62
dennis <csh9411@naver.com>
2021-04-27 12:59:12 +0900

    직각삼각형 여부

---
+++ baekjoon4153.py
@@ -0,0 +1,12 @@
+import math
+
+while True:
+    x = list(map(int, input().split()))
+    max_num =max(x)
+    if sum(x) ==0:
+        break
+    x.remove(max_num)
+    if x[0]**2 + x[1]**2 == max_num**2:
+        print("right")
+    else:
+        print("wrong")
