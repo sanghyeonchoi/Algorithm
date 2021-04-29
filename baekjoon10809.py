ebb962be12ff9e126805245e852f07bd2f5a765f
dennis <csh9411@naver.com>
2021-04-29 23:01:08 +0900

    이전 코드와 다른 코드

--- baekjoon10809.py
+++ baekjoon10809.py
@@ -1,5 +1,13 @@
-import string
+# import string
 
-a = input()
-for i in string.ascii_lowercase:
-    print(a.find(i), end=" ")
+# a = input()
+# for i in string.ascii_lowercase:
+#     print(a.find(i), end=" ")
+
+
+
+s = input()
+
+for i in range(26):
+    ch = chr(i+ord('a'))
+    print(s.find(ch), end=" ")
