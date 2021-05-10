9eb8878225261006c66928d9d0388c17bc8ecf7e
dennis <csh9411@naver.com>
2021-05-10 23:15:46 +0900

    n과 m (2)

---
+++ baekjoon15650.py
@@ -0,0 +1,14 @@
+import sys
+n,m = map(int, input().split())
+
+a = [0]*m
+def go(index, start, n,m):
+    if index == m:
+        sys.stdout.write(' '.join(map(str,a))+'\n')
+        return
+    for i in range(start,n+1):
+        a[index] = i
+        go(index+1, i+1, n, m)
+
+go(0,1,n,m)
+
