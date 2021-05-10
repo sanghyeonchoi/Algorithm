b23cff980fef394a1c0ca04a9579ed6a86ec9c1b
dennis <csh9411@naver.com>
2021-05-10 23:42:46 +0900

    중복가능한 n 과 m

---
+++ baekjoon15651.py
@@ -0,0 +1,14 @@
+import sys
+
+n,m  = map(int, input().split())
+
+a = [0]*m
+
+def go(index , n ,m):
+    if index == m:
+        sys.stdout.write(' '.join(map(str,a))+'\n')
+        return
+    for i in range(1,n+1):
+        a[index] = i
+        go(index+1, n, m)
+go(0,n,m)
