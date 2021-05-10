2aa12b213d006b40bf34db9be7f8a22ba6dc9f99
dennis <csh9411@naver.com>
2021-05-11 00:33:19 +0900

    정해진 숫자 오름차순 수열 만들기

---
+++ baekjoon15655.py
@@ -0,0 +1,16 @@
+import sys
+
+n,m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+a= [0]*m
+
+def go(index, start, n, m):
+    if index == m:
+        sys.stdout.write(' '.join(map(str, a))+'\n')
+        return
+    for i in range(start, n):
+        a[index] = num[i]
+        go(index+1, i+1,n,m)
+
+go(0,0,n,m)
