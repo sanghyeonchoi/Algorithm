9a8f6981cf9915108bbedba147dfdca7c11b9cd3
dennis <csh9411@naver.com>
2021-05-26 08:42:33 +0900

    카잉 달력

---
+++ baekjoon6064.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for _ in range(n):
+    m,n,x,y = map(int, input().split())
+
+    x -= 1
+    y -= 1
+    k = x
+    while k < n*m:
+        if k%n == y:
+            print(k+1)
+            break
+        k += m
+    else:
+        print(-1)
