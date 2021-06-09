cc71ee8d648da6d163397659df1fd20527bb377c
dennis <csh9411@naver.com>
2021-06-09 12:15:34 +0900

    이진탐색 응용문제

---
+++ 떡볶이떡만들기.py
@@ -0,0 +1,18 @@
+n,m = list(map(int,input().split()))
+array = list(map(int,input().split()))
+
+result = 0
+start = 0
+end = max(array)
+while start <= end:
+    total = 0
+    mid = (start+end)//2
+    for x in array:
+        if x>mid:
+            total += x-mid
+    if total < m:
+        end = mid -1
+    else:
+        result = mid
+        start = mid +1
+print(result)
