2c945ff44f6c3ea98a70a112eeae381cb28d34a8
dennis <csh9411@naver.com>
2021-06-10 14:13:17 +0900

    대문자 찾기 with JS

---
+++ 대문자찾기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let answer = 0;
+  for (let x of s) {
+    if (x === x.toUpperCase()) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
+function solution2(s) {
+  let answer = 0;
+  for (let x of s) {
+    let num = x.charCodeAt();
+    if (num >= 65 && num <= 90) answer++;
+  }
+  return answer;
+}
+let str = "KoreaTimeGood";
+console.log(solution(str));
