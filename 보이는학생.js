e74c647bf66abb0390917b4a79bfeab4a6ec05a6
dennis <csh9411@naver.com>
2021-06-10 17:02:27 +0900

    보이는학생 with JS

---
+++ 보이는학생.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  let answer = 0;
+  let cur = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > cur) {
+      cur = arr[i];
+      answer++;
+    }
+  }
+  return answer;
+}
+
+let arr = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(arr));
